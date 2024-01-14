504b821fe7435325717c385b129f4131ead4d5d5
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-01-14 20:46:10 +0530

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import TimeTableReport from "./printableItems/TimeTableReport";
-
-const selectedDays = ["Monday", "Tuesday", "Wednesday"];
-
 export default function Home() {
   return <main className="h-full"></main>;
 }
